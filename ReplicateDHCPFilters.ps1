7f6d5384d4efb5ab9762a088972a277f0bd36478
Nick Rodriguez <Nick.Rodriguez@dhgllp.com>
2015-09-09 17:14:18 -0400

    Changed get and add filter cmd's to use Invoke-Command.

--- ReplicateDHCPFilters.ps1
+++ ReplicateDHCPFilters.ps1
@@ -1,3 +1,12 @@
+# source server that the lists will come from
+$sourceServer = 'DHCP01'
+
+# list of destination servers to export filter list to
+$servers = @(
+'server01',
+'server02'
+)
+
 # displays the verbose messages which can help troubleshoot
 $VerbosePreference = "Continue"
 
@@ -18,26 +27,24 @@ if (Test-Path $log_file) {
     $log_file = New-Item -type file $log_file
 }
 
-# source server that the lists will come from
-$sourceServer = 'DHCP01'
-
-# list of destination servers to export filter list to
-$servers = @(
-'server01',
-'server02'
-)
+# script block to get a filter list
+$getListScript = {
+    param ($server, $listType)
+    Get-DhcpServerv4Filter -ComputerName $server -List $listType
+}
 
 try {
-    # export the Allow List and make sure its count matches the server's filter
-    Write-Verbose -message "Exporting Allow List from $sourceServer..."
-    $allow_list = Get-DhcpServerv4Filter -ComputerName $sourceServer -List Allow
+    # export the Allow List and get its item count
+    Write-Verbose "Exporting Allow List from $sourceServer..."
+    $allow_list = Invoke-Command -ComputerName $sourceServer -ScriptBlock $getListScript -ArgumentList $sourceServer, Allow
     $allow_count = $allow_list.Count
 
     # if the list is empty, skip this
     if ($allow_count) {
 
-        if ($allow_count -eq (Get-DhcpServerv4Filter -ComputerName $sourceServer -List Allow).Count) {
-            Write-Verbose -message "Successfully exported $allow_count items!"
+        # compare source list count to the list we have
+        if ($allow_count -eq (Invoke-Command -ComputerName $sourceServer -ScriptBlock $getListScript -ArgumentList $sourceServer, Allow).Count) {
+            Write-Verbose "Successfully exported $allow_count items!"
             Add-Content $log_file "$time : Exported $allow_count Allow items"
         } else {
             $Host.UI.WriteErrorLine("ERROR: Something went horribly wrong when exporting the Allow List from $sourceServer...")
@@ -45,7 +52,7 @@ try {
         }
 
     } else {
-        Write-Verbose -message "The Allow List was empty and will not be imported."
+        Write-Warning "The Allow List was empty and will not be imported."
         Add-Content $log_file "$time : Allow list was empty"
     }
 } catch {
@@ -55,16 +62,17 @@ try {
 
 
 try {
-    # export the Deny List and make sure its count matches the server's filter
-    Write-Verbose -message "Exporting Deny List from $sourceServer..."
-    $deny_list = Get-DhcpServerv4Filter -ComputerName $sourceServer -List Deny
+    # export the Deny List and get its item count
+    Write-Verbose "Exporting Deny List from $sourceServer..."
+    $deny_list = Invoke-Command -ComputerName $sourceServer -ScriptBlock $getListScript -ArgumentList $sourceServer, Deny
     $deny_count = $deny_list.Count
 
     # if the list is empty, skip this
     if ($deny_count) {
 
-        if ($deny_count -eq (Get-DhcpServerv4Filter -ComputerName $sourceServer -List Deny).Count) {
-            Write-Verbose -message "Successfully exported $deny_count items!"
+        # compare source list count to the list we have
+        if ($deny_count -eq (Invoke-Command -ComputerName $sourceServer -ScriptBlock $getListScript -ArgumentList $sourceServer, Deny).Count) {
+            Write-Verbose "Successfully exported $deny_count items!"
             Add-Content $log_file "$time : Exported $deny_count Deny items"
         } else {
             $Host.UI.WriteErrorLine("ERROR: Something went horribly wrong when exporting the Deny List from $sourceServer...")
@@ -72,12 +80,18 @@ try {
         }
 
     } else {
-        Write-Verbose -message "The Deny List was empty and will not be imported."
+        Write-Warning "The Deny List was empty and will not be imported."
         Add-Content $log_file "$time : Deny list was empty"
     }
 } catch {
     $Host.UI.WriteErrorLine("ERROR: Something went horribly wrong when exporting the Deny List from $sourceServer...")
     exit 999
+}
+
+# script block to add a filter list
+$addListScript = {
+    param ($server, $listType)
+    Add-DhcpServerv4Filter -ComputerName $server -List $listType -Force
 }
 
 foreach ($destinationServer in $servers) {
@@ -87,8 +101,8 @@ foreach ($destinationServer in $servers) {
         # keep trying to overwrite the allow and deny lists on the destination server until the filter count
         # matches the filter count from the source server's lists
         Write-Verbose "Importing Allow List on $destinationServer..."
-        while ($allow_list.Count -ne (Get-DhcpServerv4Filter -ComputerName $destinationServer -List Allow).Count) {
-            $allow_list | Add-DhcpServerv4Filter -ComputerName $destinationServer -List Allow –Force
+        while ($allow_list.Count -ne (Invoke-Command -ComputerName $destinationServer -ScriptBlock $getListScript -ArgumentList $destinationServer, Allow).Count) {
+            $allow_list | Invoke-Command -ComputerName $destinationServer -ScriptBlock $addListScript -ArgumentList $destinationServer, Allow
             Write-Warning "Allow List import failed... retrying..."
         }
         Write-Verbose "Import successful!"
@@ -100,8 +114,8 @@ foreach ($destinationServer in $servers) {
         # keep trying to overwrite the allow and deny lists on the destination server until the filter count
         # matches the filter count from the source server's lists
         Write-Verbose "Importing Deny List on $destinationServer..."
-        while ($deny_list.Count -ne (Get-DhcpServerv4Filter -ComputerName $destinationServer -List Deny).Count) {
-            $deny_list | Add-DhcpServerv4Filter -ComputerName $destinationServer -List Deny –Force
+        while ($deny_list.Count -ne (Invoke-Command -ComputerName $destinationServer -ScriptBlock $getListScript -ArgumentList $destinationServer, Deny).Count) {
+            $deny_list | Invoke-Command -ComputerName $destinationServer -ScriptBlock $addListScript -ArgumentList $destinationServer, Deny
             Write-Warning "Deny List import failed... retrying..."
         }
         Write-Verbose "Import successful!"
@@ -110,5 +124,3 @@ foreach ($destinationServer in $servers) {
     Write-Verbose "Import on $destinationServer complete!"
     Add-Content $log_file "$time : Import on $destinationServer complete"
 }
-
-pause
