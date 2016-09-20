bd49c936df8a7a733e0fdaa3e7bf03149f1e8f15
Nick Rodriguez <Nick.Rodriguez@dhgllp.com>
2016-09-20 16:59:51 -0400

    Added speech synth option.

--- Send-CatFactMessage.ps1
+++ Send-CatFactMessage.ps1
@@ -5,8 +5,8 @@
     .DESCRIPTION 
         Send a random cat fact to any number of computers and all users or a specific user. Supports credential passing.
     .EXAMPLE 
-        Send-CatFactMessage
-        Sends cat fact message to all users on localhost.
+        Send-CatFactMessage -PlayAudio
+        Sends cat fact message to all users on localhost and outputs fact through speakers.
     .EXAMPLE 
         Get-ADComputer -Filter * | Send-CatFactMessage -UserName JDoe -Credential (Get-Credential)
         Send cat fact to jDoe on all AD computers. Prompt user for credentials to run command with.
@@ -17,6 +17,8 @@
         The computer name to execute against. Default is local computer.
     .PARAMETER UserName 
         The name the user to display the message to. Default is all users.
+    .PARAMETER PlayAudio
+        Use Windows Speech Synthesizer to output the fact using text to speech.
     .PARAMETER Credential
         The credential object to execute the command with.
     #>
@@ -34,20 +36,44 @@
         [string]$UserName = '*',
 
         [Parameter(Mandatory = $false)]
+        [switch]$PlayAudio,
+
+        [Parameter(Mandatory = $false)]
         [PSCredential]$Credential
     )
 
     $CatFact = (ConvertFrom-Json (Invoke-WebRequest -Uri 'http://catfacts-api.appspot.com/api/facts')).facts
 
     if ($pscmdlet.ShouldProcess("User: $UserName, Computer: $ComputerName", "Send cat fact, $CatFact")) {
+        $ScriptBlock = {
+            param (
+                [string]$UserName,
+
+                [string]$CatFact,
+
+                [bool]$PlayAudio = $false
+            )
+
+            msg $UserName $CatFact
+
+            if ($PlayAudio) {
+                Add-Type -AssemblyName System.Speech
+                $SpeechSynth = New-Object System.Speech.Synthesis.SpeechSynthesizer
+                $SpeechSynth.Speak($CatFact)
+            }
+        }
+
         if ($Credential) {
             Write-Verbose "Sending cat fact using credential $($Credential.UserName)"
 
-            Invoke-Command -ComputerName $ComputerName -ScriptBlock { msg $args[0] $args[1] } `
-                -ArgumentList $UserName, $CatFact -Credential $Credential
+            Invoke-Command -ComputerName $ComputerName -ScriptBlock $ScriptBlock `
+                -ArgumentList $UserName, $CatFact, $PlayAudio -AsJob -Credential $Credential
         } else {
-            Invoke-Command -ComputerName $ComputerName -ScriptBlock { msg $args[0] $args[1] } `
-                -ArgumentList $UserName, $CatFact
+            Invoke-Command -ComputerName $ComputerName -ScriptBlock $ScriptBlock `
+                -ArgumentList $UserName, $CatFact, $PlayAudio -AsJob
         }
+
+        Get-Job | Wait-Job | Receive-Job
+        Get-Job | Remove-Job
     }
 }
