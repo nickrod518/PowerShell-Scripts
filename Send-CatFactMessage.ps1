96acda487423533c2a30096e751c8c04aa0465a8
Nick Rodriguez <Nick.Rodriguez@dhgllp.com>
2016-09-20 16:25:13 -0400

    Added pipeline support and help.

--- Send-CatFactMessage.ps1
+++ Send-CatFactMessage.ps1
@@ -1,7 +1,33 @@
 ﻿function Send-CatFactMessage {
+    <# 
+    .SYNOPSIS 
+        Send a cat fact to users on a computer.
+    .DESCRIPTION 
+        Send a random cat fact to any number of computers and all users or a specific user. Supports credential passing.
+    .EXAMPLE 
+        Send-CatFactMessage
+        Sends cat fact message to all users on localhost.
+    .EXAMPLE 
+        Get-ADComputer -Filter * | Send-CatFactMessage -UserName JDoe -Credential (Get-Credential)
+        Send cat fact to jDoe on all AD computers. Prompt user for credentials to run command with.
+    .EXAMPLE
+        Send-CatFactMessage -ComputerName pc1, pc2, pc3
+        Send cat fact to all users on provided computer names.
+    .PARAMETER ComputerName 
+        The computer name to execute against. Default is local computer.
+    .PARAMETER UserName 
+        The name the user to display the message to. Default is all users.
+    .PARAMETER Credential
+        The credential object to execute the command with.
+    #>
+
     [CmdletBinding(SupportsShouldProcess = $true)]
     param (
-        [Parameter(Mandatory = $false)]
+        [Parameter(
+            Mandatory = $false,
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true
+        )]
         [string[]]$ComputerName = $env:COMPUTERNAME,
 
         [Parameter(Mandatory = $false)]
