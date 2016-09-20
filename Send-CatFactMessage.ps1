ae9feb045e91baa2022e1c00a77286a7281c856d
Nick Rodriguez <Nick.Rodriguez@dhgllp.com>
2016-09-20 16:14:26 -0400

    Implemented WhatIf.

--- Send-CatFactMessage.ps1
+++ Send-CatFactMessage.ps1
@@ -11,20 +11,17 @@
         [PSCredential]$Credential
     )
 
-    
-
     $CatFact = (ConvertFrom-Json (Invoke-WebRequest -Uri 'http://catfacts-api.appspot.com/api/facts')).facts
 
-    Write-Verbose "Sending cat fact to user $UserName on computer $ComputerName"
-    Write-Verbose "Fact: $CatFact"
+    if ($pscmdlet.ShouldProcess("User: $UserName, Computer: $ComputerName", "Send cat fact, $CatFact")) {
+        if ($Credential) {
+            Write-Verbose "Sending cat fact using credential $($Credential.UserName)"
 
-    if ($Credential ) {
-        Write-Verbose "Sending cat fact using credential $($Credential.UserName)"
-
-        Invoke-Command -ComputerName $ComputerName -ScriptBlock { msg $args[0] $args[1] } `
-            -ArgumentList $UserName, $CatFact -Credential $Credential
-    } else {
-        Invoke-Command -ComputerName $ComputerName -ScriptBlock { msg $args[0] $args[1] } `
-            -ArgumentList $UserName, $CatFact
+            Invoke-Command -ComputerName $ComputerName -ScriptBlock { msg $args[0] $args[1] } `
+                -ArgumentList $UserName, $CatFact -Credential $Credential
+        } else {
+            Invoke-Command -ComputerName $ComputerName -ScriptBlock { msg $args[0] $args[1] } `
+                -ArgumentList $UserName, $CatFact
+        }
     }
 }
