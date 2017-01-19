b4bcf4a3b6d41d363fc2bb4d0e5a6d304312a490
Nick Rodriguez <nickrod518@gmail.com>
2017-01-19 13:15:10 -0500

    Script to look for service accounts in scheduled tasks and services on local or remote computers.

---
+++ Windows/Find-ServiceAccount.ps1
@@ -0,0 +1,50 @@
+﻿function Find-ServiceAccount {
+	[CmdletBinding(SupportsShouldProcess = $true)]
+	Param (
+		[Parameter(
+			Mandatory = $true,
+			ValueFromPipeline = $true,
+			ValueFromPipelineByPropertyName = $true
+		)]
+		[string[]]$Name,
+
+        [Parameter(
+			Mandatory = $true,
+			ValueFromPipeline = $true,
+			ValueFromPipelineByPropertyName = $true
+		)]
+		[string[]]$ComputerName,
+
+        [Parameter(
+			Mandatory = $false,
+			ValueFromPipeline = $true,
+			ValueFromPipelineByPropertyName = $true
+		)]
+		[PSCredential]$Credential = (Get-Credential)
+	)
+
+    $Results = @()
+
+    foreach ($ServiceAccount in $Name) {
+        $Found = New-Object -TypeName psobject -Property @{
+            Name = $ServiceAccount
+            Services = @()
+            SchTasks = @()
+        }
+
+        Write-Verbose "Searching services on [$ComputerName] for [$ServiceAccount]..."
+        $Found.Services = Get-WmiObject -Class Win32_Service -ComputerName $ComputerName -Credential $Credential |
+            Where-Object { $_.StartName -match $ServiceAccount } |
+            Select-Object DisplayName, StartName, State, ProcessId, PSComputerName
+        
+        Write-Verbose "Searching scheduled tasks on [$ComputerName] for [$ServiceAccount]..."
+        $Found.SchTasks = Invoke-Command -ComputerName $ComputerName -Credential $Credential -ScriptBlock {
+            schtasks.exe /query /s localhost /V /FO CSV | ConvertFrom-Csv
+        } | Where-Object { $_.'Run As User' -match $ServiceAccount } |
+            Select-Object TaskName, Status, 'Task To Run', Comment, 'Run As User', PSComputerName
+
+        $Results += $Found
+    }
+
+    $Results
+}
