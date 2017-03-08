188cfe0413768272ab9750be91f23569fc9c38a7
Nick Rodriguez <nickrod518@gmail.com>
2017-03-08 11:00:55 -0500

    Script to gather MFA details of MFA users.

---
+++ MSO/Get-MFAEnabledUser.ps1
@@ -0,0 +1,18 @@
+Connect-MsolService
+
+Get-MsolUser -EnabledFilter EnabledOnly -All | ForEach-Object {
+    $AuthMethods = $_.StrongAuthenticationMethods
+    $DefaultMethod = ($AuthMethods | Where-Object -Property IsDefault -EQ $true).MethodType
+
+    New-Object -TypeName psobject -Property @{
+        UserPrincipalName = $_.UserPrincipalName
+        RelyingParty = $_.StrongAuthenticationRequirements.RelyingParty
+        RememberDevicesNotIssuedBefore = $_.StrongAuthenticationRequirements.RememberDevicesNotIssuedBefore
+        State = $_.StrongAuthenticationRequirements.State
+        DefaultMethod = $DefaultMethod
+        MethodType1 = $AuthMethods[0].MethodType
+        MethodType2 = $AuthMethods[1].MethodType
+        MethodType3 = $AuthMethods[2].MethodType
+        MethodType4 = $AuthMethods[3].MethodType
+    }
+} | Export-Csv -Path ~\Downloads\MfaUsers.csv -NoTypeInformation
--- MSO/Remove-OneDriveIRM.ps1
+++ MSO/Remove-OneDriveIRM.ps1
@@ -1,7 +1,7 @@
 # These 3 lines get the Documents site from SPO for the specified user
-$webUrl = "https://company-my.sharepoint.com/personal/company”
+$webUrl = "https://company-my.sharepoint.com/personal/company"
 $clientContext = New-Object Microsoft.SharePoint.Client.ClientContext($webUrl)
-$list = $clientContext.Web.Lists.GetByTitle(“Documents")
+$list = $clientContext.Web.Lists.GetByTitle("Documents")
  
 # reset the value to the default settings
 $list.InformationRightsManagementSettings.Reset()
